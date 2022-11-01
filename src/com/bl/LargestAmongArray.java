4428090a501ad2759db3e407f1078c661e054e52
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-11-01 08:51:16 +0530

    Day 3

---
+++ src/com/bl/LargestAmongArray.java
@@ -0,0 +1,14 @@
+package com.bl;
+
+public class LargestAmongArray {
+	public static void main(String[] args) {
+		int array[] = { 4, 3, 5, 6, 7, 9, 8 };
+		int max = array[0];
+		for (int i = 0; i < array.length; i++) {
+			if (max < array[i]) {
+				max = array[i];
+			}
+		}
+		System.out.println("Lagrgest Number : " + max);
+	}
+}
